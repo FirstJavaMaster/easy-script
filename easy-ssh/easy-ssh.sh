e5f410389dc414bba6b88d76f59404b2a14908e1
仝朋成 <tongpc@idx365.com>
2019-10-09 15:54:10 +0800

    添加easy-ssh脚本

---
+++ easy-ssh/easy-ssh.sh
@@ -0,0 +1,41 @@
+# 将本地文件上传到对应的服务器上去, 简化键盘敲击次数, 不必记忆访问host和username
+
+action=$1
+file=$2
+
+user=root
+host=8.8.8.8
+
+do_ssh(){
+  ssh ${user}@${host}
+}
+
+do_scp(){
+  if [ -z "${file}" ]
+  then
+    echo "请输入要传输的文件"
+    exit 1
+  elif [ ! -e "${file}" ]
+  then
+    echo "文件不存在"
+    exit 2
+  fi
+
+  scp -r "${file}" ${user}@${host}:~
+}
+
+case ${action} in
+ssh)
+  do_ssh
+  ;;
+scp)
+  do_scp
+  ;;
+*)
+  if [ -z "${action}" ]; then
+    do_ssh
+  else
+    echo "请输入 ssh 或 scp 命令 以执行响应的功能"
+  fi
+  ;;
+esac
