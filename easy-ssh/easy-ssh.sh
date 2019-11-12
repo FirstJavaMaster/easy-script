575bcc8259a9532a28679b4781066da38072e706
仝朋成 <tongpc@idx365.com>
2019-11-12 15:52:47 +0800

    update：支持多文件上传；

--- easy-ssh/easy-ssh.sh
+++ easy-ssh/easy-ssh.sh
@@ -1,7 +1,12 @@
 # 将本地文件上传到对应的服务器上去, 简化键盘敲击次数, 不必记忆访问host和username
+# 支持多文件上传
 
+# 命令
 action=$1
-file=$2
+
+# 要传递的文件
+params=$*
+files=${params#scp}
 
 user=root
 host=8.8.8.8
@@ -11,17 +16,17 @@ do_ssh(){
 }
 
 do_scp(){
-  if [ -z "${file}" ]
+  if [ -z "${files}" ]
   then
     echo "请输入要传输的文件"
     exit 1
-  elif [ ! -e "${file}" ]
+  elif [ ! -e "${files}" ]
   then
     echo "文件不存在"
     exit 2
   fi
 
-  scp -r "${file}" ${user}@${host}:~
+  scp -r "${files}" ${user}@${host}:~
 }
 
 case ${action} in
